5609716a2dfe3acb1b3dd5f57e5ca46913c1b7cc
tochi26 <nzewitochukwu26@gmail.com>
2023-04-11 23:00:18 +0100

    implement scripts that lists all documents in the data collections

--- 0x01-NoSQL/8-all.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-''' module for task 8 '''
-
-
-def list_all(mongo_collection):
-    ''' function that lists all documents in a collection '''
-    return mongo_collection.find()
